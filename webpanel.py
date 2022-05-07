cc0248e7a0e4461f11f92db04715cd28e2a6da4c
Chris Shucksmith <chris@shucksmith.co.uk>
2022-05-07 21:54:07 +0100

    add basic html view

--- webpanel.py
+++ webpanel.py
@@ -1,19 +1,37 @@
 from aiohttp import web
+import aiohttp_jinja2
 import json
+import jinja2
 
+@aiohttp_jinja2.template('index.jinja2')
+async def handle_index(request):
+    panel = request.app["panel"]
+    return {'panel': panel.decode()}
 
-async def handle(request):
+async def handle_json(request):
     text = "Config\n\n"
     panel = request.app["panel"]
     text = text + json.dumps(panel.decode(), indent=4)
     return web.Response(text=text)
 
+@aiohttp_jinja2.template('rich.jinja2')
+async def handle_rich(request):
+    text = "Config\n\n"
+    panel = request.app["panel"]
+    return {'panel': panel.decode()}
 
 def get_web_app(mem, io, args, panel):
     app = web.Application()
-    app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+    app.add_routes([
+        web.get("/", handle_index),
+        web.get("/json", handle_json),
+        web.get("/rich", handle_rich)
+    ])
+    aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader('templates'))
 
     app["panel"] = panel
+    app["args"] = args
     return app
 
 
